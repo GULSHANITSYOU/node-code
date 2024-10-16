dca3982e13d36ec3c1f26b4b05ed50e7a4534898
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2024-10-16 17:08:07 +0530

    DP tabulation solve

--- carfleet.cpp
+++ carfleet.cpp
@@ -37,6 +37,51 @@ public:
     }
 };
 
+// dp solution 
+class Solution {
+    int solvedp(vector<int>& coins, int amount, vector<int>& dp) {
+        if (amount == 0)
+            return 0;
+        if (amount < 0)
+            return INT_MAX;
+
+        if (dp[amount] != -1)
+            return dp[amount];
+
+        int mini = INT_MAX;
+        for (const auto& coin : coins) {
+            int ans = solvedp(coins, amount - coin, dp);
+            if (ans != -1)
+                mini = min(ans, mini);
+        }
+
+        if (INT_MAX != mini)
+            dp[amount] = mini + 1;
+
+        return dp[amount];
+    }
+
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        
+        int size = coins.size() ; 
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0 ; 
+
+        
+        for(int i = 1 ; i <= amount ; i++ ){
+            for(int j = 0 ; j < size ; j++){
+                if(i - coins[j] >= 0 && dp[i-coins[j]] != INT_MAX)
+                dp[i]  = min(dp[i],1+dp[i-coins[j]]); 
+            }
+        }
+
+
+        return dp[amount] != INT_MAX ? dp[amount] : -1 ; 
+    }
+};
+
+
 int main(){
 
 }
